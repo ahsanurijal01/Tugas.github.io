11152796d426bfe6bbd832ff19e5f5b7cbe611b7
ahsanurijal01 <argames4444@gmail.com>
2023-12-15 11:01:26 +0700

    belajar mebuat controller

--- routes/web.php
+++ routes/web.php
@@ -13,82 +13,86 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
-    return view('index');
-});
+Route::get('mahasiswa', [mahasiswacontroller::class, 'index']);
 
-Route::get('/profile', function(){
-    return view('profile');
-});
 
-Route::get('/mahasiswa', function(){
-    $npm = [123,124,125,126];
-    $nama =['jono','joni','juna','jena'];
-    $jumlah = count($npm);
-    return view('mahasiswa',compact('npm','jumlah', 'nama'));
-});
 
-Route::get('/about', function(){
-    return view('about');
-});
+// Route::get('/', function(){
+//     return view('index');
+// });
 
-Route::get('/tugas1', function(){
-    return view('tugas1');
-});
+// Route::get('/profile', function(){
+//     return view('profile');
+// });
 
-Route::get('/table1', function(){
-    return view('Pertemuan5/table1');
-});
+// Route::get('/mahasiswa', function(){
+//     $npm = [123,124,125,126];
+//     $nama =['jono','joni','juna','jena'];
+//     $jumlah = count($npm);
+//     return view('mahasiswa',compact('npm','jumlah', 'nama'));
+// });
 
-Route::get('/perulangan', function(){
+// Route::get('/about', function(){
+//     return view('about');
+// });
 
-    $nilai_awal = 1;
-    $mobi_rusak =5;
-    $jumlah_mobil =10;
-    while ($nilai_awal <= $jumlah_mobil) {
-       // if ($nilai_awal> $mobi_rusak){
-       //     echo " Mobil ke-" . $nilai_awal . 'dalam kondisi rusak<br>';
+// Route::get('/tugas1', function(){
+//     return view('tugas1');
+// });
 
-        //}else{
-        //   echo "Mobil ke-" . $nilai_awal . ' beropersai dengan baik<br>';
+// Route::get('/table1', function(){
+//     return view('Pertemuan5/table1');
+// });
 
-        //}
-        if ($nilai_awal <= $mobi_rusak){
-            echo "Mobil ke-" . $nilai_awal . 'beroperasi dengan baik!<br>';
-        }
+// Route::get('/perulangan', function(){
 
-        $nilai_awal++;
-    }
+//     $nilai_awal = 1;
+//     $mobi_rusak =5;
+//     $jumlah_mobil =10;
+//     while ($nilai_awal <= $jumlah_mobil) {
+//        // if ($nilai_awal> $mobi_rusak){
+//        //     echo " Mobil ke-" . $nilai_awal . 'dalam kondisi rusak<br>';
 
-    echo '<hr>';
-    for ($i=$mobi_rusak + 1; $i <= $jumlah_mobil; $i++){
-        echo "Mobil ke-" . $i . 'dalam kondisi rusak!<br>';
-    }
+//         //}else{
+//         //   echo "Mobil ke-" . $nilai_awal . ' beropersai dengan baik<br>';
 
-});
+//         //}
+//         if ($nilai_awal <= $mobi_rusak){
+//             echo "Mobil ke-" . $nilai_awal . 'beroperasi dengan baik!<br>';
+//         }
 
-Route::get('/table2', function(){
-    $npm = ['123' ,'124', '125'];
-    $nama = ['ghh', 'njk', 'ssd',];
-    $jumlah = count($npm);
-    return view('Pertemuan5/table2', compact('npm', 'jumlah'));
-});
+//         $nilai_awal++;
+//     }
 
-Route::get('/form', function(){
-    return view('form');
-});
+//     echo '<hr>';
+//     for ($i=$mobi_rusak + 1; $i <= $jumlah_mobil; $i++){
+//         echo "Mobil ke-" . $i . 'dalam kondisi rusak!<br>';
+//     }
 
-Route::get('tugas2', function(){
-    return view('pertemuan5/tugas2');
-});
+// });
 
-Route::get('/array', function(){
-    // Infinite loop
-    $nilai_awal =1;
-    while ($nilai_awal <=100){
-        echo 'Hello Laravel '.$nilai_awal.' X<br>';
-        $nilai_awal++;
+// Route::get('/table2', function(){
+//     $npm = ['123' ,'124', '125'];
+//     $nama = ['ghh', 'njk', 'ssd',];
+//     $jumlah = count($npm);
+//     return view('Pertemuan5/table2', compact('npm', 'jumlah'));
+// });
 
-    }
+// Route::get('/form', function(){
+//     return view('form');
+// });
 
-});
+// Route::get('tugas2', function(){
+//     return view('pertemuan5/tugas2');
+// });
+
+// Route::get('/array', function(){
+//     // Infinite loop
+//     $nilai_awal =1;
+//     while ($nilai_awal <=100){
+//         echo 'Hello Laravel '.$nilai_awal.' X<br>';
+//         $nilai_awal++;
+
+//     }
+
+// });
